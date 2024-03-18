0f3e574e1f15703d00f880f7d279a197642d7004
jahardesty <81397442+jahardesty@users.noreply.github.com>
2024-03-18 13:30:04 -0400

    fetch to show in HTML

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 "use strict";
-console.log("hello");
+
 
 fetch(
-  "http://api.weatherapi.com/v1/current.json?key=ae1558409c2a4c3390a163852241503&q=90245"
+  "http://api.weatherapi.com/v1/current.json?key=ae1558409c2a4c3390a163852241503&q=46750"
 )
   .then((res) => {
     return res.json();
   })
   .then((data) => {
+    const markupLocation = `The current tempurature in <li>${data.location.name}, ${data.location.region} is</li>`;
+    const markupCurrent = `<li>${data.current.temp_f}</li>`;
+    document.querySelector('ul').insertAdjacentHTML('beforeend', markupLocation);
+    document.querySelector('ul').insertAdjacentHTML('beforeend', markupCurrent);
     console.log(data.current.temp_f);
+    console.log(data);
   });
 
 // ae1558409c2a4c3390a163852241503;
