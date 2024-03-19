9dc410c68ffd88f3521a341155d53a7a9b22ba65
jahardesty <81397442+jahardesty@users.noreply.github.com>
2024-03-18 21:40:03 -0400

    got HTML capture and return working

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
 "use strict";
 
+function getZipCode() {
+  let zipCode = document.getElementById("zipcode").value;
+  console.log(zipCode);
 
-fetch(
-  "http://api.weatherapi.com/v1/current.json?key=ae1558409c2a4c3390a163852241503&q=46750"
-)
-  .then((res) => {
-    return res.json();
-  })
-  .then((data) => {
-    const markupLocation = `The current tempurature in <li>${data.location.name}, ${data.location.region} is</li>`;
-    const markupCurrent = `<li>${data.current.temp_f}</li>`;
-    document.querySelector('ul').insertAdjacentHTML('beforeend', markupLocation);
-    document.querySelector('ul').insertAdjacentHTML('beforeend', markupCurrent);
-    console.log(data.current.temp_f);
-    console.log(data);
-  });
+  fetch(
+    `http://api.weatherapi.com/v1/current.json?key=ae1558409c2a4c3390a163852241503&q=${zipCode}`
+  )
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      const locationAndTempMarkup = `
+    <div> The current temperature in ${data.location.name}, ${data.location.region} is ${data.current.temp_f}℉ 
+    </div>`;
+      //   const markupLocation = `The current temperature in <li>${data.location.name}, ${data.location.region} is</li>`;
+      //   const markupCurrent = `<li>${data.current.temp_f}℉</li>`;
+      document
+        .querySelector("div.weather-info")
+        .insertAdjacentHTML("beforeend", locationAndTempMarkup);
+      document
+        .querySelector("div.weather-info")
+        .insertAdjacentHTML("beforeend", markupCurrent);
+      //   console.log(data.current.temp_f);
+      //   console.log(data);
+    });
+}
 
+getZipCode();
+
+//here is my API key for http://api.weatherapi.com
 // ae1558409c2a4c3390a163852241503;
