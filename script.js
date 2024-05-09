a18293d72f40f21fa58bfc907602bc79157d1ad3
jahardesty <81397442+jahardesty@users.noreply.github.com>
2024-05-08 22:00:27 -0400

    if statements

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 function getZipCode() {
   let zipCode = document.getElementById("zipcode").value;
-  console.log(zipCode);
+  // console.log(zipCode);
 
   fetch(
     `http://api.weatherapi.com/v1/current.json?key=ae1558409c2a4c3390a163852241503&q=${zipCode}`
@@ -14,25 +14,53 @@ function getZipCode() {
       console.log(data);
       const tempF = data.current.temp_f;
       const condition = data.current.condition.code;
+      const isDay = data.current.is_day;
+      let timeOfDay;
+      let currentCondition;
       const container = document.querySelector(".container");
       container.className = "container";
       container.classList.add(getBackgroundImageClass(tempF));
-      const locationAndTempMarkup = `
+      // if (isDay === 1) {
+      //   timeOfDay = "day";
+      //   // currentCondition =  ;
+      // } else {
+      //   timeOfDay = "night";
+      // }
+      fetch("weatherData.json")
+        .then((response) => response.json())
+        .then((weatherData) => {
+          const weatherCode = weatherData.find(
+            (entry) => entry.code === condition
+          );
+          const weatherDescriptionDay = weatherCode.day;
+          const weatherDescriptionNight = weatherCode.night;
+          const weatherIconCode = weatherCode.icon;
+
+          if (isDay === 1) {
+            timeOfDay = "day";
+            currentCondition = weatherCode.day;
+          } else {
+            timeOfDay = "night";
+            currentCondition = weatherCode.night;
+          }
+          console.log(weatherCode);
+          console.log(timeOfDay);
+          console.log(currentCondition);
+
+          const displayCondition = `
+    <div> <h5> ${currentCondition}</h5></div>`;
+          document
+            .querySelector("div.weather-info")
+            .insertAdjacentHTML("afterend", displayCondition);
+
+          const locationAndTempMarkup = `
     <div> <h3> ${data.location.name}, ${data.location.region} </h3></div>
     <div> <h4> ${data.current.temp_f}°F</h4></div>`;
-      document
-        .querySelector("div.weather-info")
-        .insertAdjacentHTML("afterbegin", locationAndTempMarkup);
-  
-  fetch('weatherData.json')
-    .then(response => response.json())
-    .then(weatherData => {
-      // const conditionCode = condition;
-      const weatherCode = weatherData.find(entry => entry.code === condition);
-      console.log(weatherCode);
-    })
-  });
-
+          document
+            .querySelector("div.weather-info")
+            .insertAdjacentHTML("afterbegin", locationAndTempMarkup);
+        });
+    });
   function getBackgroundImageClass(tempF) {
     if (tempF >= 50) {
       return "warm";
@@ -42,24 +70,23 @@ function getZipCode() {
       return "container";
     }
   }
-  function showClearPageButton (){
-    let clearButton = document.getElementById('clearPage');
+  function showClearPageButton() {
+    let clearButton = document.getElementById("clearPage");
     clearButton.style.display = "block";
   }
-showClearPageButton();
+  showClearPageButton();
 
-  function hideSearchInput () {
-    let searchBar = document.getElementById('zipcode');
+  function hideSearchInput() {
+    let searchBar = document.getElementById("zipcode");
     searchBar.style.display = "none";
- }
-hideSearchInput(); 
+  }
+  hideSearchInput();
 
- function hideSearchButton () {
-    let searchButton = document.getElementById('searchButton');
+  function hideSearchButton() {
+    let searchButton = document.getElementById("searchButton");
     searchButton.style.display = "none";
- }
- hideSearchButton();
-
+  }
+  hideSearchButton();
 }
 
 function clearPage() {
@@ -70,6 +97,3 @@ function clearPage() {
 }
 
 clearPage();
-
-//here is my API key for http://api.weatherapi.com
-// ae1558409c2a4c3390a163852241503;
